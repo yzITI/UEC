2c5ee91c120e41376d9006755a4a51cf1b3945ab
Tina Ji <tinayutong0310@gmail.com>
2021-11-20 15:35:00 +1100

    Update xyzsas.zh.js

--- xyzsas.zh.js
+++ xyzsas.zh.js
@@ -7,3 +7,4 @@ UEC['XYZSAS-0003'] = '身份验证失败' // username/password error
 UEC['XYZSAS-0004'] = '身份验证失败' // Aauth login error
 UEC['XYZSAS-0005'] = '账户不存在或已经激活' // activation error
 UEC['XYZSAS-0006'] = '原密码不正确' // change password error
+UEC['XYZSAS-0007'] = '第三方登录绑定失败' // link sas and aauth failed
